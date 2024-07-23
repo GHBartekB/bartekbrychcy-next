974aa1e9cf49a45d3539e720dfc888a627cd2b5b
GHBartekB <bbrychcy@onet.eu>
2024-07-23 15:55:52 +0200

    Add nav i18n dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,7 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-
-import Container from '@/components/ui/Container';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Bartek Brychcy',
-  description: 'Portfolio CV Projekty',
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <nav>
-          <Container>Nawigacja</Container>
-        </nav>
-        <div>{children}</div>
-        <footer>
-          <Container>Stopka</Container>
-        </footer>
-      </body>
-    </html>
-  );
+  return children;
 }
